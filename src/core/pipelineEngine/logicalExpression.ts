5477ac2f687f5564025a9ad59c463b82364e2422
Elran Shefer <elran777@gmail.com>
2024-12-16 22:03:34 +0200

    fixup: allow strings in both side of boolean functions

--- src/core/pipelineEngine/logicalExpression.ts
+++ src/core/pipelineEngine/logicalExpression.ts
@@ -81,8 +81,8 @@ const processStringFunction = (args: FactorType[], context: Context, func: (a: s
     }
 
     const [left, right] = args;
-    if (left.type !== "columnRef" || (right.type !== "string" && right.type !== "columnRef")) {
-        throw new Error(`Invalid argument types for function - expected: (columnRef, string | columnRef), got ${left.type} and ${right.type}`);
+    if ((left.type !== "string" && left.type !== "columnRef") || (right.type !== "string" && right.type !== "columnRef")) {
+        throw new Error(`Invalid argument types for function - expected: (string | columnRef, string | columnRef), got ${left.type} and ${right.type}`);
     }
 
     const leftValue = processFieldValue(context, left);
