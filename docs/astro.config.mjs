46115e36ef005cb543e598b4d2ecc829b82eb882
Elran Shefer <elran777@gmail.com>
2025-01-04 19:10:06 +0200

    chore: added cd for docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://cruncher.iamshobe.com',
+	base: '/docs/',
 	integrations: [
 		starlight({
 			title: 'Cruncher',
