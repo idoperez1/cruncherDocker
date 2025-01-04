b69c07d9db15b6ae87f60a674cf88e34e3c91bb8
Elran Shefer <elran777@gmail.com>
2025-01-04 17:29:58 +0200

    chore: replace logo + start docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Cruncher',
+			logo: {
+				light: '/src/assets/cruncher_full_logo.png',
+				dark: '/src/assets/cruncher_full_logo.png',
+				replacesTitle: true,
+			},
+			social: {
+				github: 'https://github.com/IamShobe/cruncher',
+			},
+			favicon: './src/assets/favicon.ico',
+			customCss: ["./src/assets/landing.css"],
+			sidebar: [
+				{
+					label: 'QQL Reference',
+					autogenerate: { directory: 'qql_reference' },
+				},
+			],
+		}),
+	],
+});
