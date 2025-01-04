c1b495b6f37311bdfdafacd1aa6e03d78d1fe03f
Elran Shefer <elran777@gmail.com>
2025-01-04 20:33:52 +0200

    fixup: increase content length

--- src/bundle/LocalContent.tsx
+++ src/bundle/LocalContent.tsx
@@ -3,7 +3,7 @@ import MainContent from '~core/MainContent';
 
 export const LocalContent = ({query}: {query: string}) => {
     return (
-        <main style={{width: "100%", height: 500, display: "flex"}}>
+        <main style={{width: "100%", height: 700, display: "flex", overflow: "hidden"}}>
             <MainContent controller={MockController}
                 initialQuery={query}
             />
